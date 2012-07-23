1a6027d2ba371b87bce70c59ff1024aea651447a
abdul <abdul@mongolab.com>
2012-07-23 14:49:51 -0700

    list-servers/list-clusters formatting

--- mongoctl/mongoctl.py
+++ mongoctl/mongoctl.py
@@ -263,7 +263,7 @@ def list_servers_command(parsed_options):
     servers = sorted(servers, key=lambda s: s.get_id())
     bar = "-"*80
     print bar
-    formatter = "%-14s %-40s %s"
+    formatter = "%-25s %-40s %s"
     print formatter % ("_ID", "DESCRIPTION", "CONNECT TO")
     print bar
 
@@ -326,7 +326,7 @@ def list_clusters_command(parsed_options):
 
     bar = "-"*80
     print bar
-    formatter = "%-14s %-40s %s"
+    formatter = "%-25s %-40s %s"
     print formatter % ("_ID", "DESCRIPTION", "MEMBERS")
     print bar
 
