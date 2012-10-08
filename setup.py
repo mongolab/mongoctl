07de881c9ae2e9a9c7a0f391eb2443fd90c95ead
abdul <abdul@mongolab.com>
2012-10-08 13:15:39 -0700

    Setting version to 0.3.3

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ def mongoctl_post_install():
 ###############################################################################
 setup(
     name='mongoctl',
-    version='0.3.2',
+    version='0.3.3',
     author='MongoLab team',
     author_email='team@mongolab.com',
     description='MongoDB command line utility',
