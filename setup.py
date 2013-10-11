5fd466e39964c8da948493c79cf6ae8598c3df12
abdul <abdul@mongolab.com>
2013-10-10 17:52:04 -0700

    mongoctl is at 0.5.0

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ def mongoctl_post_install():
 ###############################################################################
 setup(
     name='mongoctl',
-    version='0.3.7',
+    version='0.5.0',
     author='MongoLab team',
     author_email='team@mongolab.com',
     description='MongoDB command line utility',
