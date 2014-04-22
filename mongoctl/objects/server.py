9ae0553ddd04f18b0480be6686feda360a0d49ac
abdul <abdul@objectlabs.com>
2014-04-22 12:45:19 -0700

    auth to local db using admin if version >= 2.6.0

--- mongoctl/objects/server.py
+++ mongoctl/objects/server.py
@@ -391,9 +391,12 @@ class Server(DocumentWrapper):
 
         # if there is no login user for this database then use admin db unless
         # it was specified not to
-        if (not never_auth_with_admin and
+        # ALSO use admin if this is 'local' db for mongodb >= 2.6.0
+        if ((not never_auth_with_admin and
                 not login_user and
-                    dbname not in ["admin", "local"]):
+                    dbname not in ["admin", "local"]) or
+                (dbname == "local" and
+                     not users.server_supports_local_users(self))):
             # if this passes then we are authed!
             admin_db = self.get_db("admin", retry=retry)
             return admin_db.connection[dbname]
