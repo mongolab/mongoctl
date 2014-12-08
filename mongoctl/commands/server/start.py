3da2334ca83a0c505c1a40cf8644495b5d4b6e28
abdul <abdul@objectlabs.com>
2014-12-08 12:37:01 -0800

    prompt message tweak

--- mongoctl/commands/server/start.py
+++ mongoctl/commands/server/start.py
@@ -294,8 +294,8 @@ def prompt_init_replica_cluster(replica_cluster,
                                 suggested_primary_server):
 
     prompt = ("Do you want to initialize replica set cluster '%s' using server"
-              " '%s'? (type 'n' (NO) unless you are absolutely sure you"
-              " should be initializing this replica set)?" %
+              " '%s'? [type 'n' (NO) unless you are absolutely sure you"
+              " should be initializing this replica set]?" %
               (replica_cluster.id, suggested_primary_server.id))
 
     def init_repl_func():
