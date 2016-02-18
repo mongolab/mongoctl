10382a20c6dbb32a3f492e7a67a65ff0b9e15b1b
abdul <abdul@objectlabs.com>
2016-02-17 22:05:11 -0800

    revert

--- mongoctl/commands/common/status.py
+++ mongoctl/commands/common/status.py
@@ -20,7 +20,7 @@ def status_command(parsed_options):
         log_info("Status for server '%s':" % id)
         if isinstance(server, MongodServer) and server.is_arbiter_server():
             # XXX? TODO we use to pass admin=False to arbiters but maybe this should be fine now
-            status = server.get_status(admin=True)
+            status = server.get_status(admin=False)
         else:
             status = server.get_status(admin=True)
     else:
